cb5b5f11a672a84961ed3d386bfbd984c661db11
aj83854 <36650362+aj83854@users.noreply.github.com>
2018-09-22 18:51:30 -0500

    Constructor creates genesis block; fleshed out methods

--- blockchain.py
+++ blockchain.py
@@ -6,12 +6,32 @@ from time import time
 class Blockchain(object):
 
     def __init__(self):
+        self.current_transactions = []
         self.chain = []
+
+        # Creates a new genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new block on the blockchain.
+
+        :param proof: <int> The 'proof' given by the PoW algorithm.
+        :param previous_hash: (Optional) <str> Hash of the previous block.
+        :return: <dict> New Block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
         self.current_transactions = []
 
-    def new_block(self):
-        # Creates a new block and adds it to the chain
-        pass
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -30,12 +50,22 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
-    @staticmethod
-    def hash(block):
-        # Hashes a block
-        pass
-
     @property
     def last_block(self):
-        # Returns the last block in the chain
-        pass
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block.
+
+        We ensure that the dictionary is ordered using 'sort_keys';
+        otherwise, we'll end up with inconsistent hashes.
+
+        :param block: <dict> Block
+        :return: <str>
+
+        """
+
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
