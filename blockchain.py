dc07c7a56db2145cc85a512db13316d30ac5520d
aj83854 <36650362+aj83854@users.noreply.github.com>
2018-09-23 12:47:27 -0500

    Added option to choose server port when running from CLI

--- blockchain.py
+++ blockchain.py
@@ -296,5 +296,13 @@ def full_chain():
     }
     return jsonify(response), 200
 
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
