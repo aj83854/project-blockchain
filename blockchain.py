80dfb788e45d0747dc5e118130f43c70114f4f7a
aj83854 <36650362+aj83854@users.noreply.github.com>
2018-09-22 21:43:04 -0500

    Added mining endpoint

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,6 @@
 import hashlib
 import json
-from textwrap import dedent
+
 from time import time
 from uuid import uuid4
 
@@ -120,7 +120,31 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We'll mine a new block"
+    # We run the proof_of_work algorithm to get the next proof:
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # We receive a reward for finding the proof.
+    # The sender is '0' to signify that this node has mined a new coin:
+    blockchain.new_transaction(
+        sender='0',
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Forge the new block by adding it to the chain:
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': 'New block forged',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
@@ -147,4 +171,4 @@ def full_chain():
     return jsonify(response), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=5000)
