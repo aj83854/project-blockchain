12f3080004e9fd60b903dbcece7673e9c6d6fd28
aj83854 <36650362+aj83854@users.noreply.github.com>
2018-09-23 12:09:02 -0500

    Added endpoints for adding neighbouring nodes & resolving conflicts.

--- blockchain.py
+++ blockchain.py
@@ -192,6 +192,7 @@ node_identifier = str(uuid4()).replace('-', '')
 # Instantiates the Blockchain
 blockchain = Blockchain()
 
+
 @app.route('/mine', methods=['GET'])
 def mine():
     # We run the proof_of_work algorithm to get the next proof:
@@ -220,6 +221,43 @@ def mine():
     }
     return jsonify(response), 200
 
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return 'Error: Please supply a valid list of nodes', 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added!'
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced!',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative.',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
+
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
     values = request.get_json()
@@ -235,6 +273,7 @@ def new_transaction():
 
     response = {'message': f'Transaction will be added to block {index}'}
     return jsonify(response), 201
+
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
